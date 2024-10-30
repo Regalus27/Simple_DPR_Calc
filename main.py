b5219e92623cdb52ba1953f0241f9333ad4b9856
Regalus27 <16656788+Regalus27@users.noreply.github.com>
2024-10-29 21:48:34 -0500

    Fixed xlabel

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ from itertools import cycle
 import matplotlib.pyplot as plt
 
 def main():
-    doug_warlock = DougWarlockWarMagicUpdated(20).get_character()
-    # doug_warlock.graph_damage_per_combat()
-
-    doug_ek = DougEldritchKnight(20).get_character()
-    # doug_ek.graph_damage_per_combat(20)
-
     characters = []
+
+    doug_warlock = DougWarlockWarMagicUpdated(20).get_character()
     characters.append(doug_warlock)
+    
+    doug_ek = DougEldritchKnight(20).get_character()
     characters.append(doug_ek)
+    
     graph_damage_per_character(characters)
 
 def graph_damage_per_character(characters: list[Character]):
@@ -22,14 +21,13 @@ def graph_damage_per_character(characters: list[Character]):
 
     # There's probably a super easy way to generate this, but it was faster to type it
     plot_x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] # Levels
-    # plot_x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
     for character in characters:
         plot_y = []
         for i in range(1, len(character.combats) + 1):
             plot_y.append(character.combats[i-1].calc_damage_per_round()) # DPR
         plt.plot(plot_x, plot_y, color=next(cycle_colors), label=character.name)
     
-    plt.xlabel("Label")
+    plt.xlabel("Level")
     plt.ylabel("Average DPR")
     plt.suptitle("Various Doug DPRs")
     plt.legend()
